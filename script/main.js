f16c669f2685fd30eda9e7542a082278da986c44
asdf.digital <93361701+asdfdigital@users.noreply.github.com>
2023-01-08 15:10:45 +0530

    update

--- script/main.js
+++ script/main.js
@@ -1,11 +1,48 @@
 let menuBtn = document.querySelector('.menu-btn');
 let navMenu = document.querySelector('nav');
 let yellowLine = document.querySelector('.yellow-line');
+let navLink = document.querySelectorAll('nav ul li a');
+let header = document.querySelector('header');
+let body = document.querySelector('body');
+let seeWorkBtn = document.querySelector('#see-my-work');
+let portfolio = document.querySelector('.portfolio')
+let splash = document.querySelector('splash');
 
 menuBtn.addEventListener('click', menuOpen);
 
 function menuOpen() {
     navMenu.classList.toggle('nav-open');
-    yellowLine.classList.toggle('yellow-line-shift')
+    yellowLine.classList.toggle('yellow-line-shift');
+    body.classList.toggle('overflow-hidden');
 }
 
+navLink.forEach(element => {
+    element.addEventListener('click', ()=> {
+        navMenu.classList.remove('nav-open');
+        yellowLine.classList.remove('yellow-line-shift');
+        body.classList.toggle('overflow-hidden');
+    })
+});
+
+seeWorkBtn.addEventListener('click', ()=> {
+    portfolio.scrollIntoView();
+})
+
+window.addEventListener('scroll', (e)=> {
+    if (e.path[1].pageYOffset > 400) {
+        header.classList.add('position-fixed');
+    }else {
+        header.classList.remove('position-fixed');
+    }
+})
+
+
+
+
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    loop: false,
+    disableRightClick: true,
+    maxZoom: 3
+});
