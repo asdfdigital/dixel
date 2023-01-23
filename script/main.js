63f61faab3bc14892205336aaf0b672624cf22de
asdf.digital <93361701+asdfdigital@users.noreply.github.com>
2023-01-23 12:44:26 +0530

    some updates

--- script/main.js
+++ script/main.js
@@ -1,62 +1,16 @@
-// let menuBtn = document.querySelector('.menu-btn');
-// let navMenu = document.querySelector('nav');
-// let yellowLine = document.querySelector('.yellow-line');
-// let navLink = document.querySelectorAll('nav ul li a');
-// let header = document.querySelector('header');
-// let body = document.querySelector('body');
-// let seeWorkBtn = document.querySelector('#see-my-work');
-// let portfolio = document.querySelector('.portfolio')
-// let splash = document.querySelector('.splash');
-
-// window.addEventListener('load', ()=> {
-//     console.log('done');
-//     splash.style.display = 'none';
-// });
-
-// menuBtn.addEventListener('click', menuOpen);
-
-// function menuOpen() {
-//     navMenu.classList.toggle('nav-open');
-//     yellowLine.classList.toggle('yellow-line-shift');
-//     body.classList.toggle('overflow-hidden');
-// }
-
-// navLink.forEach(element => {
-//     element.addEventListener('click', ()=> {
-//         navMenu.classList.remove('nav-open');
-//         yellowLine.classList.remove('yellow-line-shift');
-//         body.classList.toggle('overflow-hidden');
-//     })
-// });
-
-// seeWorkBtn.addEventListener('click', ()=> {
-//     portfolio.scrollIntoView();
-// })
-
-// window.addEventListener('scroll', (e)=> {
-//     if (e.path[1].pageYOffset > 400) {
-//         header.classList.add('position-fixed');
-//     }else {
-//         header.classList.remove('position-fixed');
-//     }
-// })
-
-
-
-
-
-
-// let gallery = new SimpleLightbox('.gallery a', {
-//     loop: false,
-//     disableRightClick: true,
-//     maxZoom: 3
-// });
-
 const menuBtn = document.querySelector('.menu-btn');
 const navMenu = document.querySelector('nav');
 const yellowLine = document.querySelector('.yellow-line');
 const navLink = document.querySelectorAll('nav ul li a');
+
 const body = document.querySelector('body');
+
+let toggleBtn = document.querySelector('.dark-mode-toggle');
+let toggleHandle = document.querySelector('.toggler');
+let logo = document.querySelectorAll('.logo');
+let menuBtnDualLines = document.querySelector('.menu-btn-wrapper .menu-btn .dual-lines');
+
+// navbar fixed
 
 window.addEventListener('scroll', ()=> {
     if (window.scrollY > 220) {
@@ -65,6 +19,8 @@ window.addEventListener('scroll', ()=> {
         document.querySelector('header').classList.remove('header-fixed') 
     }
 })
+
+// menu
 
 menuBtn.addEventListener('click', menuOpen);
 
@@ -78,6 +34,37 @@ navLink.forEach(element => {
         yellowLine.classList.remove('yellow-line-shift');
     })
 })
+
+// dark mode
+
+if (localStorage.getItem('dark-mode', true)) {
+    darkModeFunc();
+}
+
+toggleBtn.addEventListener('click', ()=> {
+    darkModeFunc();
+})
+
+function darkModeFunc () {
+    console.log('true');
+    toggleHandle.classList.toggle('toggle-on');
+    document.documentElement.toggleAttribute("data-theme-dark");
+    if (document.documentElement.hasAttribute('data-theme-dark')) {
+        logo.forEach(element => {
+            element.setAttribute('src', 'images/assets/dixel_logo_white.svg');
+        });
+        menuBtnDualLines.setAttribute('src', 'images/assets/dual-lines-white.svg');
+        localStorage.setItem('dark-mode', true);
+    }else {
+        logo.forEach(element => {
+            element.setAttribute('src', 'images/assets/dixel_logo.svg');
+        });
+        menuBtnDualLines.setAttribute('src', 'images/assets/dual-lines.svg');
+        localStorage.removeItem('dark-mode', true);
+    };
+}
+
+// primary button
 
 document.querySelector('.primary-button').addEventListener('click', ()=> {
     document.querySelector('#portfolio').scrollIntoView();
