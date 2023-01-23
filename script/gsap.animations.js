4702f64be27b1e065fdecb3434a4cf6512079130
asdf.digital <93361701+asdfdigital@users.noreply.github.com>
2023-01-23 11:46:06 +0530

    animation adjustments

--- script/gsap.animations.js
+++ script/gsap.animations.js
@@ -1,6 +1,6 @@
 let nltl = gsap.timeline({repeatDelay: 1, delay: 0});
 
-nltl.from('.gradiel', {duration: 0.6, x: -400}, ">-2")
+nltl.from('.gradiel', {duration: 0.6, x: -400, delay: 0.5})
 nltl.from('.n1', {duration: 0.2, x: 40, opacity: 0})
 nltl.from('.n2', {duration: 0.2, x: 40, opacity: 0})
 nltl.from('.n3', {duration: 0.2, x: 40, opacity: 0})
@@ -17,43 +17,48 @@ tl.from(".t3", {duration: 0.3, y: -20, opacity: 0, ease: "slow(0.7, 0.7, false)"
 tl.from(".r4", {duration: 0.3, x: 30, opacity: 0});
 tl.from(".avatar-wrapper", {duration: 0.5, x: 30, opacity: 0, ease: "slow(0.7, 0.7, false)"})
 
-const io = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.intersectionRatio > 0) {
-        nltl.play()
-      } else {
-        nltl.seek(0)
-        nltl.pause()
-      }
-    })
-  })
+// const io = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.intersectionRatio > 0) {
+//         nltl.play()
+//       } else {
+//         nltl.seek(0)
+//         nltl.pause()
+//       }
+//     })
+//   })
+
+// const boxElList = document.querySelectorAll('.nav');
+// boxElList.forEach((el) => {
+//   io.observe(el);
+// })
+
+
+document.querySelector('.menu-btn').addEventListener('click', ()=> {
+  if (document.querySelector('.nav').classList.contains("nav-open")) {
+    nltl.play()
+  } else {
+    nltl.seek(1)
+    nltl.reverse()
+  } 
+}) 
   
-  
-  const boxElList = document.querySelectorAll('.nav');
-  boxElList.forEach((el) => {
-    io.observe(el);
-  })
 
 
 
-  const op = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.intersectionRatio > 0) {
-        tl.play()
-        console.log('play')
-      } else {
-        tl.seek(0)
-        tl.pause()
-      }
-    })
+const op = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      tl.play()
+    } 
   })
-  
-  
-  const aniObser = document.querySelectorAll('.aniObser');
-  aniObser.forEach((el) => {
-    op.observe(el);
-  })
-  
+})
+
+const aniObser = document.querySelectorAll('.aniObser');
+aniObser.forEach((el) => {
+  op.observe(el);
+})
+
 
     
 
